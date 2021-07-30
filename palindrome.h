86c6e0a5a93ba66bc0195973efb2b17cf06ef607
lvn3668 <55122106+lvn3668@users.noreply.github.com>
2021-07-30 11:55:26 -0400

    Delete palindrome.h
    Replaced with palindromes.h

--- palindrome.h
+++
@@ -1,6 +0,0 @@
-// Author: Lalitha Viswanathan
-// Affiliation: Tata Consultancy Services
-long int len;
-char* seq;
-long int minlength, maxlength,gaplength,mismatches,match,mismatch;
-void findpalindromesandinvertedrepeats();
